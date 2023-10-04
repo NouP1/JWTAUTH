df58c32c06b7e3e75df78ac86a20747c5c0bf2f3
NouP1 <smolen43ret@gmail.com>
2023-10-04 23:57:24 +0500

    auth

---
+++ server/router/index.js
@@ -0,0 +1,14 @@
+
+
+const Router = require('express').Router;
+const userController = require('../controllers/user-controller');
+const router = new Router();
+
+router.post('/registration',userController.registration);
+router.post('/login',userController.login);
+router.post('/logout', userController.logout);
+router.post('/activate/:link',userController.activate);
+router.post('/refresh',userController.refresh);
+router.post('/users',userController.getUsers);
+
+module.exports = router
